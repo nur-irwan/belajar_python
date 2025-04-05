992c7990c5b5e9b6c9aa5ada1ac935f207d7d467
Irwan <nurirwan183@gmail.com>
2025-04-06 00:16:34 +0700

    first python

---
+++ latihan3.py
@@ -0,0 +1,5 @@
+# membuat input nama, umur , dan hobi
+nama = input("Masukkkan Nama :")
+umur = input("Masukkaan Umur :")
+hobi = input ("masukkan hobi:")
+print(f"Halo Nama saya {nama}, Umur saya {umur} tahun, Hobi Saya {hobi}")
---
+++ latihan4.py
@@ -0,0 +1,4 @@
+# membuat calculator sederhana 2 angka
+a = int(input("a :"))
+b = int(input("b :"))
+print(f"Hasilnya kali = {a*b}, Hasil Bagi = {a/b}, Hasil kurang = {a-b}, Hasil tambah = {a+b}")
