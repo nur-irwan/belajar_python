fe1cc0826d0501cf5da025f2ace5f67a3f057b8f
Irwan <nurirwan183@gmail.com>
2025-04-06 01:05:35 +0700

    lanjutan python

---
+++ belajar_percabangan1.py
@@ -0,0 +1,7 @@
+umur = int(input("Masukkan Umur:"))
+if umur < 18:
+    print("Kamu masih dibawah umur.")
+elif umur < 60:
+    print("Kamu Sudah Dewasas.")
+else:
+    print("Kamu Sudah Tua.")
---
+++ latihan_percabangan2.py
@@ -0,0 +1,9 @@
+nilai = int(input("Masukkan Nilai Ujian:"))
+if nilai >= 90:
+    print("Nilai Kamu : A")
+elif nilai >= 80:
+    print("Nilai Kamu: B")
+elif nilai >=70:
+    print("Nilai Kamu : C")
+else:
+    print("Nilai Kamu: D")
